c235213fe8199535f0efa771b0f6850b5430d3c0
EunjoungAhn <aej9300@gmail.com>
2022-12-10 00:47:03 +0900

    버전 변경 및 초기 DB 데이터 작업 파일 추가, 자주 사용하는 uitls 함수 파일 추가

---
+++ lib/data/data.dart
@@ -0,0 +1,134 @@
+
+class Food{
+  int id;
+  int date;
+  int type;
+  int kcal;
+  int time;
+  String memo;
+  String image;
+
+  Food({this.id, this.date, this.type, this.kcal, this.time, this.memo,
+      this.image});
+
+  //food를 데이터 베이스에서 불러올때 사용
+  factory Food.fromDB(Map<String, dynamic> data){
+    return Food(
+        id: data["id"],
+        date: data["date"],
+        type: data["type"],
+        kcal: data["kcal"],
+        time: data["time"],
+        memo: data["memo"],
+        image: data["image"],
+    );
+  }
+  
+  // 객체를 실제로 저장할때는 map으로 변환해서 저장
+  Map<String, dynamic> toMap(){
+    return {
+      "id": this.id,
+      "date": this.date,
+      "type": this.type,
+      "kcal": this.kcal,
+      "time": this.time,
+      "memo": this.memo,
+      "image": this.image,
+    };
+  }
+}
+
+class Workout{
+  int id;
+  int date;
+  int time;
+  int calorie;
+  int intense; // 강도
+  int part; // 부위
+
+  String name;
+  String memo;
+
+  Workout({this.id, this.date, this.time, this.calorie, this.intense, this.part,
+      this.name, this.memo});
+
+  factory Workout.fromDB(Map<String, dynamic> data){
+    return Workout(
+      id: data["id"],
+      date: data["date"],
+      time: data["time"],
+      calorie: data["calorie"],
+      intense: data["intense"],
+      part: data["part"],
+      name: data["name"],
+      memo: data["memo"],
+    );
+  }
+
+  Map<String, dynamic> toMap(){
+    return {
+      "id": this.id,
+      "date": this.date,
+      "time": this.time,
+      "calorie": this.calorie,
+      "intense": this.intense,
+      "part": this.part,
+      "name": this.name,
+      "memo": this.memo,
+    };
+  }
+}
+
+class EyeBody{
+  int id;
+  int date;
+  String image;
+  String memo;
+
+  EyeBody({this.id, this.date, this.image, this.memo});
+
+  factory EyeBody.fromDB(Map<String, dynamic> data){
+    return EyeBody(
+      id: data["id"],
+      date: data["date"],
+      image: data["image"],
+      memo: data["memo"],
+    );
+  }
+
+  Map<String, dynamic> toMap(){
+    return {
+      "id": this.id,
+      "date": this.date,
+      "image": this.image,
+      "memo": this.memo,
+    };
+  }
+}
+
+class Weight{
+  int date;
+  int weight;
+  int fat;
+  int muscle;
+
+  Weight({this.date, this.weight, this.fat, this.muscle});
+
+  factory Weight.fromDB(Map<String, dynamic> data){
+    return Weight(
+      date: data["date"],
+      weight: data["weight"],
+      fat: data["fat"],
+      muscle: data["muscle"],
+    );
+  }
+
+  Map<String, dynamic> toMap(){
+    return {
+      "date": this.date,
+      "weight": this.weight,
+      "fat": this.fat,
+      "muscle": this.muscle,
+    };
+  }
+}
---
+++ lib/data/database.dart
@@ -0,0 +1,272 @@
+
+import 'package:dietapp/data/data.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static final _databaseName = "dietapp.db";
+  static final _databaseVersion = 1;
+  static final foodTable = "food";
+  static final workoutTable = "workout";
+  static final bodyTable = "body";
+  static final weightTable = "weight";
+
+  DatabaseHelper._privateConstructor();
+
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+
+  static Database _database;
+
+  Future<Database> get database async {
+    // DB가 null이 아니면 기존의 DB를 넘겨 주세요.
+    if (_database != null) return _database;
+    // DB가 없으면 DB 초기화 함수를 실행
+    _database = await _initDatabase();
+    return _database;
+  }
+  // 없으면 데이터 베이스를 생성하겠다.
+  _initDatabase() async {
+    var databasePath = await getDatabasesPath(); // DB의 경로를 가져오기
+    String path = join(databasePath, _databaseName);
+    return await openDatabase(path, version: _databaseVersion, onCreate: _onCreate,
+        onUpgrade: _onUpgrade);
+  }
+
+  // 앱이 처음 실행될때 데이터 베이스를 생성
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+    CREATE TABLE IF NOT EXISTS $foodTable (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      date INTEGER DEFAULT 0,
+      type INTEGER DEFAULT 0,
+      kcal INTEGER DEFAULT 0,
+      time INTEGER DEFAULT 0,
+      image String,
+      memo String
+    )
+    ''');
+
+    await db.execute('''
+    CREATE TABLE IF NOT EXISTS $workoutTable (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      date INTEGER DEFAULT 0,
+      time INTEGER DEFAULT 0,
+      calorie INTEGER DEFAULT 0,
+      intense INTEGER DEFAULT 0,
+      part INTEGER DEFAULT,
+      name String,
+      memo String
+    )
+    ''');
+
+    await db.execute('''
+    CREATE TABLE IF NOT EXISTS $bodyTable (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      date INTEGER DEFAULT 0,
+      image String,
+      memo String
+    )
+    ''');
+
+    await db.execute('''
+    CREATE TABLE IF NOT EXISTS $weightTable (
+      date INTEGER DEFAULT 0,
+      weight INTEGER DEFAULT 0,
+      fat INTEGER DEFAULT 0,
+      muscle INTEGER DEFAULT 0,
+    )
+    ''');
+  }
+
+  Future _onUpgrade(Database db, int oldVersion, int newVersion) async {}
+
+  //음식
+  Future<int> insertFood(Food food) async {
+    Database db = await instance.database;
+
+    if(food.id == null) {
+      //생성
+      final _map = food.toMap();
+
+      return await db.insert(foodTable, _map);
+    }else{
+      //변경
+      final _map = food.toMap();
+      return await db.update(foodTable, _map, where: "id = ?", whereArgs: [food.id]);
+    }
+
+  }
+
+  Future<List<Food>> getFoodByDate(int date) async {
+    Database db = await instance.database;
+    List<Food> foods = [];
+
+    // foodTable 특정 date의 데이터를 가져와라
+    var query = await db.query(foodTable, where: "date = ?", whereArgs: [date]);
+
+    // 리스트에 다 담아주고
+    for(var q in query){
+      foods.add(Food.fromDB(q));
+    }
+
+    return foods;
+  }
+
+  Future<List<Food>> queryAllFood() async {
+    Database db = await instance.database;
+    List<Food> foods = [];
+
+    // foodTable 있는 데이터를 다 가져와라
+    var query = await db.query(foodTable);
+
+    // 리스트에 다 담아주고
+    for(var q in query){
+      foods.add(Food.fromDB(q));
+    }
+
+    return foods;
+  }
+
+  //운동
+  Future<int> insertWorkout(Workout workout) async {
+    Database db = await instance.database;
+
+    if(workout.id == null) {
+      //생성
+      final _map = workout.toMap();
+
+      return await db.insert(workoutTable, _map);
+    }else{
+      //변경
+      final _map = workout.toMap();
+      return await db.update(workoutTable, _map, where: "id = ?", whereArgs: [workout.id]);
+    }
+
+  }
+
+  Future<List<Workout>> getWorkoutByDate(int date) async {
+    Database db = await instance.database;
+    List<Workout> workouts = [];
+
+    // workoutTable 특정 date의 데이터를 가져와라
+    var query = await db.query(workoutTable, where: "date = ?", whereArgs: [date]);
+
+    // 투두 리스트에 다 담아주고
+    for(var q in query){
+      workouts.add(Workout.fromDB(q));
+    }
+
+    return workouts;
+  }
+
+  Future<List<Workout>> queryAllWorkout() async {
+    Database db = await instance.database;
+    List<Workout> workouts = [];
+
+    // workoutTable 있는 데이터를 다 가져와라
+    var query = await db.query(workoutTable);
+
+    // 투두 리스트에 다 담아주고
+    for(var q in query){
+      workouts.add(Workout.fromDB(q));
+    }
+
+    return workouts;
+  }
+
+  //바디
+  Future<int> insertEyeBody(EyeBody body) async {
+    Database db = await instance.database;
+
+    if(body.id == null) {
+      //생성
+      final _map = body.toMap();
+
+      return await db.insert(bodyTable, _map);
+    }else{
+      //변경
+      final _map = body.toMap();
+      return await db.update(bodyTable, _map, where: "id = ?", whereArgs: [body.id]);
+    }
+
+  }
+
+  Future<List<EyeBody>> getEyeBodyByDate(int date) async {
+    Database db = await instance.database;
+    List<EyeBody> eyeBodys = [];
+
+    // eyeBodyTable 특정 date의 데이터를 가져와라
+    var query = await db.query(bodyTable, where: "date = ?", whereArgs: [date]);
+
+    // 리스트에 다 담아주고
+    for(var q in query){
+      eyeBodys.add(EyeBody.fromDB(q));
+    }
+
+    return eyeBodys;
+  }
+
+  Future<List<EyeBody>> queryAllEyeBody() async {
+    Database db = await instance.database;
+    List<EyeBody> eyeBodys = [];
+
+    // eyeBodyTable 있는 데이터를 다 가져와라
+    var query = await db.query(bodyTable);
+
+    // 리스트에 다 담아주고
+    for(var q in query){
+      eyeBodys.add(EyeBody.fromDB(q));
+    }
+
+    return eyeBodys;
+  }
+
+  //무게
+  Future<int> insertWeight(Weight weight) async {
+    Database db = await instance.database;
+
+    List<Weight> _d = await getWeightByDate(weight.date);
+
+    if(_d.isEmpty) {
+      //생성
+      final _map = weight.toMap();
+
+      return await db.insert(weightTable, _map);
+    }else{
+      //변경
+      final _map = weight.toMap();
+      return await db.update(weightTable, _map, where: "date = ?", whereArgs: [weight.date]);
+    }
+
+  }
+
+  Future<List<Weight>> getWeightByDate(int date) async {
+    Database db = await instance.database;
+    List<Weight> weights = [];
+
+    // weightTable 특정 date의 데이터를 가져와라
+    var query = await db.query(weightTable, where: "date = ?", whereArgs: [date]);
+
+    // 리스트에 다 담아주고
+    for(var q in query){
+      weights.add(Weight.fromDB(q));
+    }
+
+    return weights;
+  }
+
+  Future<List<Weight>> queryAllWeight() async {
+    Database db = await instance.database;
+    List<Weight> weights = [];
+
+    // weightTable 있는 데이터를 다 가져와라
+    var query = await db.query(weightTable);
+
+    // 리스트에 다 담아주고
+    for(var q in query){
+      weights.add(Weight.fromDB(q));
+    }
+
+    return weights;
+  }
+}
