f5fe5710d8d16e91a2cd1535a15bbd3402b0009c
EunjoungAhn <aej9300@gmail.com>
2022-12-11 00:34:01 +0900

    그리트 뷰 작업 리스트 클릭시 작동하게 작성

--- lib/style.dart
+++ lib/style.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 Color mainColor = Color(0xFF81d0d6);
 Color bgColor = Colors.white;
-Color ibgColor = Colors.grey;
+Color ibgColor = Colors.grey.withOpacity(0.1);
 Color txtColor = Colors.black;
 Color iTxtColor = Colors.black38;
 double cardSize = 130;
--- lib/view/food.dart
+++ lib/view/food.dart
@@ -36,14 +36,17 @@ class _FoodAddPageState extends State<FoodAddPage> {
             }
             else if( index == 1){
               return Container(
+                margin: EdgeInsets.symmetric(horizontal: 16),
                 child: Column(
                   children: [
                     Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Text("식사시간"),
                         Text("오후 11:32"),
                       ],
                     ),
+                    Container(height: 12,),
                     GridView.count(
                       // 그리드 뷰 이지만 움직이지 않게 설정
                       physics: NeverScrollableScrollPhysics(),
@@ -51,23 +54,81 @@ class _FoodAddPageState extends State<FoodAddPage> {
                       children: List.generate(
                         // utils 파일에 작성한 list 타입의 텍스트를 하나씩 만들어 준다.
                           mealTime.length, (_index) {
-                            return Container(
-                              child: Text(mealTime[_index],
-                              style: TextStyle(
-                                color: food.type == _index ? Colors.white : iTxtColor,
-                              ),),
-                              // 클릭시 활성화 같은 표시를 위해 설정
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(8),
-                                color: food.type == _index ? mainColor : ibgColor,
+                            return InkWell(
+                              child: Container(
+                                alignment: Alignment.center,
+                                child: Text(mealTime[_index],
+                                style: TextStyle(
+                                  color: food.type == _index ? Colors.white : iTxtColor,
+                                ),),
+                                // 클릭시 활성화 같은 표시를 위해 설정
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8),
+                                  color: food.type == _index ? mainColor : ibgColor,
+                                ),
                               ),
+                              // 클릭시 index로 변경 가능하도록
+                              onTap: () {
+                                setState(() {
+                                  food.type = _index;
+                                });
+                              },
                             );
                         }
                         ),
                       crossAxisCount: 4, // 4개 만큼만 표시 넘으면 밑으로
                       crossAxisSpacing: 4,
                       mainAxisSpacing: 4,
-                      childAspectRatio: 2,
+                      childAspectRatio: 2.5,
+                    )
+                  ],
+                ),
+              );
+            }
+            else if( index == 2){
+              return Container(
+                margin: EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("식단평가"),
+                      ],
+                    ),
+                    Container(height: 12,),
+                    GridView.count(
+                      // 그리드 뷰 이지만 움직이지 않게 설정
+                      physics: NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      children: List.generate(
+                        // utils 파일에 작성한 list 타입의 텍스트를 하나씩 만들어 준다.
+                          mealType.length, (_index) {
+                        return InkWell(
+                          child: Container(
+                            alignment: Alignment.center,
+                            child: Text(mealType[_index],
+                              style: TextStyle(
+                                color: food.type == _index ? Colors.white : iTxtColor,
+                              ),),
+                            // 클릭시 활성화 같은 표시를 위해 설정
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              color: food.type == _index ? mainColor : ibgColor,
+                            ),
+                          ),
+                          onTap: () {
+                            setState(() {
+                              food.type = _index;
+                            });
+                          },
+                        );
+                      }
+                      ),
+                      crossAxisCount: 4, // 4개 만큼만 표시 넘으면 밑으로
+                      crossAxisSpacing: 4,
+                      mainAxisSpacing: 4,
+                      childAspectRatio: 2.5,
                     )
                   ],
                 ),
