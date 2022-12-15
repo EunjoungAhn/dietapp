817636686aa3e739778d369374fb3f63985b6cd5
EunjoungAhn <aej9300@gmail.com>
2022-12-15 19:14:18 +0900

    통계 화면 UI 페이지 제작 후, 그래프 적용

--- lib/main.dart
+++ lib/main.dart
@@ -45,8 +45,11 @@ class _MyHomePageState extends State<MyHomePage> {
 
   // 메인화면에서 각각의 데이터를 표현할 데이터 저장 변수 선언
   List<Workout> workouts = [];
+  List<Workout> allWorkouts = [];
   List<Food> foods = [];
+  List<Food> allFoods = [];
   List<EyeBody> bodies = [];
+  List<EyeBody> allBodies = [];
   List<Weight> weight = [];
   List<Weight> weights = [];
 
@@ -58,6 +61,10 @@ class _MyHomePageState extends State<MyHomePage> {
     bodies = await dbHelper.queryEyeBodyByDate(_d);
     weight = await dbHelper.queryWeightByDate(_d);
     weights = await dbHelper.queryAllWeight();
+    //통계를 위한 변수 선언
+    allFoods = await dbHelper.queryAllFood();
+    allWorkouts = await dbHelper.queryAllWorkout();
+    allBodies = await dbHelper.queryAllEyeBody();
     int charIndex = 0; // 어떤 그래프를 클릭했는지
 
     //그 날짜의 몸무게가 존재한다면 불러와라
@@ -213,6 +220,8 @@ class _MyHomePageState extends State<MyHomePage> {
       return getHistoryWidget();
     }else if(currentIndex == 2){
       return getWeightWidget();
+    }else if(currentIndex == 3){
+      return getStatisticWidget();
     }
 
 
@@ -660,6 +669,157 @@ class _MyHomePageState extends State<MyHomePage> {
           return Container();
         },
         itemCount: 3,
+      ),
+    );
+  }
+  
+  // 통계 페이지
+  Widget getStatisticWidget(){
+    return Container(
+      child: ListView.builder(
+          itemBuilder: (context, index) {
+            if(index == 0){
+              List<FlSpot> spots = [];
+
+              //반복을 돌면서 그래프를 그린다.
+              for(final w in allWorkouts){
+                if(chartIndex == 0){
+                  //몸무게
+                  spots.add(FlSpot(w.date.toDouble(), w.time.toDouble()));
+                }else if(chartIndex == 1){
+                  //근육량
+                  spots.add(FlSpot(w.date.toDouble(), w.kcal.toDouble()));
+                }else{
+                  //지방
+                  spots.add(FlSpot(w.date.toDouble(), w.distance.toDouble()));
+                }
+              }
+
+             return Container(
+               child: Column(
+                 children: [
+                   Row(
+                     children: [
+                       InkWell(
+                         child: Container(
+                           child: Text("운동시간", style: TextStyle(
+                               color: chartIndex == 0 ? Colors.white : iTxtColor
+                           ),),
+                           decoration: BoxDecoration(
+                               color: chartIndex == 0 ? mainColor : ibgColor,
+                               borderRadius: BorderRadius.circular(8)
+                           ),
+                           padding: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                         ),
+                         onTap: () async {
+                           setState(() {
+                             chartIndex = 0;
+                           });
+                         },
+                       ),
+                       Container(width: 8,),
+                       InkWell(
+                         child: Container(
+                           child: Text("칼로리", style: TextStyle(
+                               color: chartIndex == 1 ? Colors.white : iTxtColor
+                           ),),
+                           decoration: BoxDecoration(
+                               color: chartIndex == 1 ? mainColor : ibgColor,
+                               borderRadius: BorderRadius.circular(8)
+                           ),
+                           padding: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                         ),
+                         onTap: () async {
+                           setState(() {
+                             chartIndex = 1;
+                           });
+                         },
+                       ),
+                       Container(width: 8,),
+                       InkWell(
+                         child: Container(
+                           child: Text("거리", style: TextStyle(
+                               color: chartIndex == 2 ? Colors.white : iTxtColor
+                           ),),
+                           decoration: BoxDecoration(
+                               color: chartIndex == 2 ? mainColor : ibgColor,
+                               borderRadius: BorderRadius.circular(8)
+                           ),
+                           padding: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                         ),
+                         onTap: () async {
+                           setState(() {
+                             chartIndex = 2;
+                           });
+                         },
+                       ),
+                     ],
+                   ),
+                   // 차트
+                   Container(
+                     height: 300,
+                     margin: EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+                     padding: EdgeInsets.symmetric(horizontal: 25, vertical: 16),
+                     decoration: BoxDecoration(
+                         color: bgColor,
+                         borderRadius: BorderRadius.circular(12),
+                         boxShadow: [
+                           BoxShadow(
+                               blurRadius: 4,
+                               spreadRadius: 4,
+                               color: Colors.black12
+                           )
+                         ]
+                     ),
+                     child: LineChart(
+                       LineChartData(
+                         lineBarsData: [
+                           LineChartBarData(
+                               spots: spots,
+                               colors:  [mainColor]
+                           )
+                         ],
+                         gridData: FlGridData(
+                             show: false
+                         ),
+                         borderData: FlBorderData(
+                             show: false
+                         ),
+                         lineTouchData: LineTouchData(
+                             touchTooltipData: LineTouchTooltipData(
+                               getTooltipItems: (spots) {
+                                 return [
+                                   LineTooltipItem(
+                                       "${spots.first.y}", TextStyle(color: mainColor)
+                                   )
+                                 ];
+                               },
+                             )
+                         ),
+                         titlesData: FlTitlesData(
+                             bottomTitles: SideTitles(
+                               showTitles: true,
+                               getTitles: (value) {
+                                 DateTime date = Utils.stringToDateTime(value.toInt().toString());
+                                 return "${date.day}일";
+                               },
+                             ),
+                             leftTitles: SideTitles(
+                               showTitles: false,
+                             )
+                         ),
+
+                       ),
+                     ),
+                   )
+                 ],
+               ),
+             );
+            }
+
+            return Container();
+          },
+        itemCount: 4,
       ),
     );
   }
